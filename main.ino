68aa68e669c747d34db851d53ea6935d1402d03c
glaera <31164368+glaera@users.noreply.github.com>
2018-04-22 21:02:47 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,6 +1,15 @@
 #include <MD_Parola.h>              //MD Parola Library
 #include <MD_MAX72xx.h>             //MD Maxx72XX Library
 #include <SPI.h>                    //SPI library for comms
+#include <ESP8266WiFi.h>
+#include <WifiUDP.h>
+#include <String.h>
+#include <Wire.h>
+#include <NTPClient.h>
+#include <Time.h>
+#include <TimeLib.h>
+#include <Timezone.h>
+
 
 #define MAX_DEVICES 4
 //ESP8266
@@ -11,6 +20,22 @@
 #define SensorPin D5
 #define LedPin D4
 
+
+WiFiClient client;
+// Define NTP properties
+#define NTP_OFFSET   60 * 60      // In seconds
+#define NTP_INTERVAL 60 * 1000    // In miliseconds
+#define NTP_ADDRESS  "ca.pool.ntp.org"  // change this to whatever pool is closest (see ntp.org)
+
+// Set up the NTP UDP client
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
+
+String date;
+String t;
+const char * days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"} ;
+const char * months[] = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"} ;
+const char * ampm[] = {"AM", "PM"} ;
 
 // arduino mega
 // #define CLK_PIN 13
@@ -28,88 +53,147 @@ int scrollSpeed=35;    // used to set text scroll speed in Parola at start
 textEffect_t scrollEffect = PA_SCROLL_LEFT; //parameters from Parola library, sets scrolling from left
 textPosition_t scrollAlign = PA_LEFT;
 
-int scrollPause = 2000; // ms of pause after finished displaying message
+int scrollPause = 1000; // ms of pause after finished displaying message
 
 #define  BUF_SIZE  75  // Maximum of 75 characters
 char curMessage[BUF_SIZE] = { "greta is the best" };  // used to hold current message
+
+
 #define slider_pin A0  // OTA or OTB pin on slider
 int slider_val;  // used to hold the slider analog value
 int slide_scroll_speed;   // used when changing scroll speed
 
+#define WLAN_SSID       "x"
+#define WLAN_PASS       "x"
 
+void LanConnect() {
+    // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+}
 
 void setup()
 {
 
- // pinMode(slider_pin, INPUT);
+    Serial.begin(9600);  
+    timeClient.begin();   // Start the NTP UDP client
+  
+
+    LanConnect();
+
     pinMode(SensorPin, INPUT);
     pinMode(LedPin,OUTPUT);
-    Serial.begin(9600);
-
+    
   
    P.begin();                                //MD Parola parameter - begin
    P.displayClear();                         //MD Parola parameter - clear the display
    P.displaySuspend(false);                  //MD Parola parameter - suspend or not?
 
   //MD Paroloa display msg using our predefined parameters
-   P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+//gennaro   P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
 
 
 }
 
+void updateTime() 
+{
+  if (WiFi.status() == WL_CONNECTED) //Check WiFi connection status
+  {   
+    date = "";  // clear the variables
+    t = "";
+    
+    // update the NTP client and get the UNIX UTC timestamp 
+    timeClient.update();
+    unsigned long epochTime =  timeClient.getEpochTime();
+
+    // convert received time stamp to time_t object
+    time_t local, utc;
+    utc = epochTime;
+
+    // Then convert the UTC UNIX timestamp to local time
+    TimeChangeRule usEDT = {"CEST", Second, Sun, Mar, 2, 60};  //UTC - 5 hours - change this as needed
+    TimeChangeRule usEST = {"CEST", First, Sun, Nov, 2, 60};   //UTC - 6 hours - change this as needed
+    Timezone usEastern(usEDT, usEST);
+    local = usEastern.toLocal(utc);
+
+    // now format the Time variables into strings with proper names for month, day etc
+    date += days[weekday(local)-1];
+    date += ", ";
+    date += months[month(local)-1];
+    date += " ";
+    date += day(local);
+    date += ", ";
+    date += year(local);
+
+    // format the time to 12-hour format with AM/PM and no seconds
+    t += hourFormat12(local);
+    t += ":";
+    if(minute(local) < 10)  // add a zero if minute is under 10
+      t += "0";
+    t += minute(local);
+    t += " ";
+    t += ampm[isPM(local)];
+
+    // Display the date and time
+    Serial.println("");
+    Serial.print("Local date: ");
+    Serial.print(date);
+    Serial.println("");
+    Serial.print("Local time: ");
+    Serial.print(t);
+
+    String myMessage = t + " "+ date+" ";
+    //char charBuf[myMessage.length() + 1];
+    myMessage.toCharArray(curMessage, myMessage.length());
+    
+    //P.print(charBuf);
+    //P.displayClear();                         //MD Parola parameter - clear the display
+    //P.displaySuspend(false);                  //MD Parola parameter - suspend or not?
+
+    //MD Paroloa display msg using our predefined parameters
+    if (P.displayAnimate()) {
+      P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
+    }
+    delay(0);
+  }
+  else // attempt to connect to wifi again if disconnected
+  {
+    WiFi.begin(WLAN_SSID, WLAN_PASS);
+    Serial.println("Connected.");
+  }
+    
+}
+
 void loop(void)
 {
-  /*
-  slider_val = analogRead(A0);  // check slider analog value
-  
-  if (slider_val > 600) {  // if slider to the left
-    
-    scrollEffect = PA_SCROLL_LEFT;  // Change direction to Left
-    
-    P.setTextEffect(scrollEffect, scrollEffect); // set new direction in Parola (OLD, NEW)
-    
-    slide_scroll_speed = map(slider_val, 1023, 0, 15, 400); // map slider value to text scroll speed
-    
-    P.setSpeed(slide_scroll_speed);  // Set new text scroll speed
-    
-  }
-  
-  
-  if (slider_val < 350) {  // if slider to the right
-    
-    scrollEffect = PA_SCROLL_RIGHT;
-    
-    P.setTextEffect(scrollEffect, scrollEffect);  
-    
-    slide_scroll_speed = map(slider_val, 1023, 0, 400, 15);
-    
-    P.setSpeed(slide_scroll_speed);
-  }
-  
-
-  if (slider_val < 600 && slider_val > 350) {  // if slider in middle
-    
-    slide_scroll_speed = 500;
-    
-    P.setSpeed(slide_scroll_speed);
-  }
-  */
 
   int sensorValue = digitalRead(SensorPin);
   bool detectedMovement = sensorValue == HIGH;
   if (detectedMovement) {
+    updateTime();
     digitalWrite(LedPin, HIGH);
-    Serial.println("Movement detected");
+ //   Serial.println("Movement detected");
   } else {
     digitalWrite(LedPin, LOW);
-    Serial.println("No movement detected");
+ //   Serial.println("No movement detected");
   }
 
   if (detectedMovement && P.displayAnimate()) // If finished displaying message
   {
-    P.displayReset();  // Reset and display it again
+ //gennaro   P.displayReset();  // Reset and display it again
+   // P.displayText(curMessage, scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
   } else if (!detectedMovement) {
-     P.displayClear();
+    P.displayClear();
+    P.displayText("", scrollAlign, scrollSpeed, scrollPause, scrollEffect, scrollEffect);
   }
 
  // delay(50);
